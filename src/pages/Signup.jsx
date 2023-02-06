dff0fd252fdb93a88f2a49978a76c6891ca3b3ca
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 04:49:01 +0900

    ✨ email 입력 핸들러

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,20 +1,36 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState();
+
+  function handleEmailInput({ target: { value } }) {
+    const emailPattern = /@/;
+    const isEmailValid = emailPattern.test(value);
+
+    setEmail(value);
+    setIsEmailValid(isEmailValid);
+  }
+
   return (
     <>
       <h1>회원가입</h1>
       <form>
         <label htmlFor="email">이메일</label>
-        <input type="text" name="" id="email" />
-        <span></span>
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={handleEmailInput}
+          data-testid="email-input"
+        />
+        {isEmailValid === false && <span>이메일 형식을 확인해주세요.</span>}
+
         <label htmlFor="password">비밀번호</label>
-        <input type="text" name="" id="password" />
+        <input id="password" type="password" />
         <span></span>
-        <label htmlFor="password-confirm">비밀번호 확인</label>
-        <input type="text" name="" id="password-confirm" />
-        <span></span>
-        <button type="button">회원가입</button>
+        <button type="submit">회원가입</button>
       </form>
       <Link to={'/signin'}>로그인</Link>
     </>
