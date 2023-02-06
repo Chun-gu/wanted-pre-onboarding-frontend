d1a6cfe4d71955cd9b27b2362442c91938a84af6
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 05:13:26 +0900

    ✨ email 입력 핸들러 구현

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Signin() {
+  const [email, setEmail] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState();
+
+  function handleEmailInput({ target: { value } }) {
+    const emailPattern = /@/;
+    const isEmailValid = emailPattern.test(value);
+
+    setEmail(value);
+    setIsEmailValid(isEmailValid);
+  }
+
   return (
     <>
       <h1>로그인</h1>
       <form>
         <label htmlFor="email">이메일</label>
-        <input type="text" name="" id="email" />
-        <span></span>
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={handleEmailInput}
+          data-testid="email-input"
+        />
+        {isEmailValid === false && <span>이메일 형식을 확인해주세요.</span>}
+
         <label htmlFor="password">비밀번호</label>
         <input type="text" name="" id="password" />
         <span></span>
