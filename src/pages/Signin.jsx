aae069359fc23805359fa3ad10e2c6609ae96d7d
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 05:27:55 +0900

    🩹 비밀번호 인풋 타입 password로 변경

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -67,7 +67,7 @@ export default function Signin() {
         <label htmlFor="password">비밀번호</label>
         <input
           id="password"
-          type="text"
+          type="password"
           value={password}
           onChange={handlePasswordInput}
           data-testid="password-input"
