94fa229276fc7528a5ad04549e9224483b56a0cf
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 07:09:38 +0900

    ✨ Todos를 api로 가져와서 출력

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { TodoItem } from '../components';
+
 export default function Todo() {
+  const [todos, setTodos] = useState();
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    async function getTodos() {
+      axios
+        .get('https://pre-onboarding-selection-task.shop/todos', {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => setTodos(response.data));
+    }
+    getTodos();
+  }, []);
+
   return (
     <>
       <h1>할 일 목록</h1>
@@ -7,7 +25,11 @@ export default function Todo() {
         <input type="text" id="todo" />
         <button type="submit">추가</button>
       </form>
-      <ul></ul>
+      <ul>
+        {todos?.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
     </>
   );
 }
