e5d7a480c2145f8ffa3d551446125dba508b996b
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 04:50:58 +0900

    ✨ password 입력 핸들러

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function Signup() {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isEmailValid, setIsEmailValid] = useState();
+  const [isPasswordValid, setIsPasswordValid] = useState();
 
   function handleEmailInput({ target: { value } }) {
     const emailPattern = /@/;
@@ -11,6 +13,13 @@ export default function Signup() {
 
     setEmail(value);
     setIsEmailValid(isEmailValid);
+  }
+
+  function handlePasswordInput({ target: { value } }) {
+    const isPasswordValid = value.length >= 8;
+
+    setPassword(value);
+    setIsPasswordValid(isPasswordValid);
   }
 
   return (
@@ -28,8 +37,17 @@ export default function Signup() {
         {isEmailValid === false && <span>이메일 형식을 확인해주세요.</span>}
 
         <label htmlFor="password">비밀번호</label>
-        <input id="password" type="password" />
-        <span></span>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={handlePasswordInput}
+          data-testid="password-input"
+        />
+        {isPasswordValid === false && (
+          <span>비밀번호는 8자 이상이어야 합니다.</span>
+        )}
+
         <button type="submit">회원가입</button>
       </form>
       <Link to={'/signin'}>로그인</Link>
