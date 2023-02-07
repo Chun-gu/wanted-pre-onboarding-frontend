bcd1985d8821fb2bde3b7eaf007a191c93aa072d
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 15:45:52 +0900

    ✨ Todo 작성 기능 추가

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,9 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { TodoItem } from '../components';
 
 export default function Todo() {
   const [todos, setTodos] = useState();
+  const todoInputRef = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const todo = todoInputRef.current.value;
+    if (!todo) return;
+
+    const token = localStorage.getItem('jwt');
+    axios
+      .post(
+        'https://pre-onboarding-selection-task.shop/todos',
+        { todo },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then(({ data }) => {
+        todoInputRef.current.value = '';
+        setTodos((prev) => [...prev, data]);
+      });
+  }
 
   useEffect(() => {
     const token = localStorage.getItem('jwt');
@@ -20,10 +42,17 @@ export default function Todo() {
   return (
     <>
       <h1>할 일 목록</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="todo">할 일 작성</label>
-        <input type="text" id="todo" />
-        <button type="submit">추가</button>
+        <input
+          id="todo"
+          ref={todoInputRef}
+          type="text"
+          data-testid="new-todo-input"
+        />
+        <button type="submit" data-testid="new-todo-add-button">
+          추가
+        </button>
       </form>
       <ul>
         {todos?.map((todo) => (
