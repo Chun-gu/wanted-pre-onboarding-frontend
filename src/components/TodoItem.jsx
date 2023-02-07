b919e0919ca042c35e57a2f64625dedbfafb0650
Chun-gu <chungu.yi92@gmail.com>
2023-02-07 21:18:20 +0900

    ✨ 수정 버튼 클릭시 ui 수정모드로 변경

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import axios from 'axios';
 
 export default function TodoItem({ id, todo, isCompleted }) {
+  const [isModifyMode, setIsModifyMode] = useState();
+
   function handleInput() {
     const token = localStorage.getItem('jwt');
     axios.put(
@@ -10,18 +13,38 @@ export default function TodoItem({ id, todo, isCompleted }) {
     );
   }
 
+  function handleModifyMode() {
+    setIsModifyMode((isModifyMode) => !isModifyMode);
+  }
+
   return (
     <li>
-      <label>
-        <input
-          type="checkbox"
-          defaultChecked={isCompleted}
-          onChange={handleInput}
-        />
-        <span>{todo}</span>
-      </label>
-      <button data-testid="modify-button">수정</button>
-      <button data-testid="delete-button">삭제</button>
+      {isModifyMode ? (
+        <>
+          <input type="text" data-testid="modify-input" />
+          <button onClick={handleModifyMode} data-testid="submit-button">
+            제출
+          </button>
+          <button onClick={handleModifyMode} data-testid="cancel-button">
+            취소
+          </button>
+        </>
+      ) : (
+        <>
+          <label>
+            <input
+              type="checkbox"
+              defaultChecked={isCompleted}
+              onChange={handleInput}
+            />
+            <span>{todo}</span>
+          </label>
+          <button onClick={handleModifyMode} data-testid="modify-button">
+            수정
+          </button>
+          <button data-testid="delete-button">삭제</button>
+        </>
+      )}
     </li>
   );
 }
