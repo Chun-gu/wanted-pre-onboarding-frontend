40dc60d48de95370b12bb7b2721c48e17f786ae3
Chun-gu <chungu.yi92@gmail.com>
2023-02-08 16:13:11 +0900

    axios 인스턴스 생성 로직 분리

---
+++ src/apis/config.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('jwt');
+
+  if (accessToken) config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+export default api;
