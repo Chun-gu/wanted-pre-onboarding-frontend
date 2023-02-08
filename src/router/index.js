e512883729db150eba28fae93080d71e39e44abd
Chun-gu <chungu.yi92@gmail.com>
2023-02-09 06:07:28 +0900

    ✨ TodoProvider 적용

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { SignUpPage, SignInPage, TodoPage, ErrorPage } from '../pages';
+import { TodoProvider } from '../contexts';
 
 export default function Router() {
   const router = createBrowserRouter([
@@ -10,7 +11,14 @@ export default function Router() {
       children: [
         { path: 'signup', element: <SignUpPage /> },
         { path: 'signin', element: <SignInPage /> },
-        { path: 'todo', element: <TodoPage /> },
+        {
+          path: 'todo',
+          element: (
+            <TodoProvider>
+              <TodoPage />
+            </TodoProvider>
+          ),
+        },
       ],
       errorElement: <ErrorPage />,
     },
