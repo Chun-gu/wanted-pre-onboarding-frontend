eef0f60bba014b33f250107f6aaff01072c1cce4
Chun-gu <chungu.yi92@gmail.com>
2023-02-10 09:51:37 +0900

    💬  authReducer 관련 상수 정의

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -5,6 +5,11 @@ export const TODO_ACTION = Object.freeze({
   delete: 'DELETE_TODO',
 });
 
+export const AUTH_ACTION = Object.freeze({
+  signIn: 'SIGN_IN',
+  signOut: 'SIGN_OUT',
+});
+
 export const ACTION = Object.freeze({
   notExist: (type) => `${type}은 존재하지 않는 액션입니다.`,
 });
