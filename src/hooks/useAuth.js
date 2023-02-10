8c5463702275e90ff470addcaa6fbd897d82d84a
Chun-gu <chungu.yi92@gmail.com>
2023-02-10 09:54:18 +0900

    ✨ useAuth 훅 생성

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+
+import { AuthStateContext, AuthDispatchContext } from '../contexts/authContext';
+
+export default function useAuth() {
+  const authStateContext = useContext(AuthStateContext);
+  const authDispatchContext = useContext(AuthDispatchContext);
+
+  if (authStateContext === undefined)
+    return new Error(
+      'authStateContext는 AuthStateContext.Provider 내부에서 사용해야 합니다.'
+    );
+
+  if (authDispatchContext === undefined)
+    return new Error(
+      'authDispatchContext는 AuthDispatchContext.Provider 내부에서 사용해야 합니다.'
+    );
+
+  return [authStateContext, authDispatchContext];
+}
