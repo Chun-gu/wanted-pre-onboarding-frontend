51698f777f71791436f99089e47e050a08dc8f9e
Chun-gu <chungu.yi92@gmail.com>
2023-02-10 09:59:56 +0900

    ✨ 권한별 리다이렉트 위한 ProtectedRoute 컴포넌트 생성

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
+export { default as Layout } from './Layout';
+export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as TodoList } from './TodoList';
 export { default as TodoItem } from './TodoItem';
